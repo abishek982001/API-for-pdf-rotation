811958dac5c361a59d7090bd7ef482b904eab1a3
abishek982001 <abishek982001@gmail.com>
2022-11-19 09:53:20 +0530

    Added comments

--- app.py
+++ app.py
@@ -7,11 +7,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # Home page
     return "Welcome to Pdf Rotater"
 
 
 @app.route("/rotate/<path:file_path>/<int:angle>/<int:page_number>")
 def rotate_single_page(file_path, angle, page_number):
+    """
+    Function to rotate a single page in pdf.
+    If angle not a multiple of 90 degree, no changes will be made in the rotated pdf.
+    If the route is not specified properly, returns 404 error.
+    Can give file name alone or entire path of file in route.
+    If all goes fine, shows the rotated pdf.
+    """
     pdf_in = open(file_path, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_in)
     pdf_writer = PyPDF2.PdfFileWriter()
@@ -32,8 +40,15 @@ def rotate_single_page(file_path, angle, page_number):
 
 @app.route("/rotate/<path:file_path>/<int:angle>/<int:start>/<int:end>")
 def rotate_multiple_page(file_path, angle, start, end):
+    """
+    Function to rotate multiple pages of pdf.
+    If angle not a multiple of 90 degree, no changes will be made in the rotated pdf.
+    If the route is not specified properly, returns 404 error.
+    Can give file name alone or entire path of file in route.
+    If all goes fine, shows the rotated pdf.
+    """
     angle = int(angle)
-    limit = set(range(int(start)-1, int(end)))
+    limit = set(range(start-1, end))
 
     pdf_in = open(file_path, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_in)
